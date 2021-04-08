5c802bc216c06dca155ac3553962ddb4eddf34fd
ig77355 <ig77355@sgh.waw.pl>
2021-04-09 01:02:17 +0200

    new

--- prime no.py
+++ prime no.py
@@ -1,7 +1,11 @@
-n = 30
-if n > 1:
-    for i in range(2, n):
-        if (n % i) == 0:
-            break
-else:
-    print(n)
+n = int(input("How many prime numbers would you like to display?\n"))
+a=0
+for i in range(2, 1000000000):
+    if a<n:
+        if i>1:
+            for b in range(2, i):
+                if (i % b) == 0:
+                    break
+            else:
+                print(i)
+                a=a+1
