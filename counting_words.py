4fc8cf1f69d93386e6b6da850d926b1c47fc605c
ig77355 <ig77355@sgh.waw.pl>
2021-04-16 13:03:26 +0200

    newest

--- counting_words.py
+++ counting_words.py
@@ -1,1 +1,15 @@
 my_file = 'text1'
+d = dict()
+
+with open(my_file, 'r') as f:
+    for line in f:
+        line = line.strip()
+        # splitting the line into words
+        words = line.split(" ")
+        for word in words:
+            if word in d:
+                d[word] = d[word] + 1
+            else:
+                d[word] = 1
+    for key in list(d.keys()):
+        print(key, ":", d[key])
